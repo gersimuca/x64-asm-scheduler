dc6a0f602f760aa892b09a77825290a5f7e10a3b
Gersi Muça <gm96key@gmail.com>
2026-09-01 21:55:14 -0400

    Update data.asm

--- src/data.asm
+++ src/data.asm
@@ -1,6 +1,16 @@
 bits 64
 
+; ============================================================
+; Read-only project data
+; ============================================================
+
 section .rodata
+
+global target_path
+global backup_path
+global success_message
+global error_message
+global empty_message
 
 target_path:
     db "/tmp/my_project", 0
@@ -13,3 +23,6 @@ success_message:
 
 error_message:
     db "x64-asm-scheduler: error", 10, 0
+
+empty_message:
+    db "x64-asm-scheduler: no regular files found", 10, 0
